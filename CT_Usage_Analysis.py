67190b2640ce23e9c4e15163e8d8cab254b1a524
David Jennings <dkj.jennings@gmail.com>
2019-07-01 11:13:01 -0700

    Partially working CT_Usage.py - need to change name

--- CT_Usage_Analysis.py
+++ CT_Usage_Analysis.py
@@ -2,6 +2,12 @@
 cutting tool usage data and file count by programmer.
 Note - Toolist file location is hard coded.'''
 
+# ToDo
+# Fix usage count to include only target files
+# Add holder column
+# Wrap in class
+# Add programmer list
+
 import os
 import re
 from collections import Counter
@@ -13,135 +19,136 @@ from openpyxl import load_workbook
 from openpyxl.styles import Border, Side, Alignment, Font, NamedStyle
 
 
-class Parse_Files:
-    ''' Class to parse through files in specified folder, search
-    pattern, and return match'''
-
-    # Class variables
-
-    new_dict = {}
-    single_list = {}
-    result_dict = {}
-    machine = ''
-
-    def __init__(self, folder, pattern):
-        self.folder = folder
-        self.pattern = pattern
-        os.chdir(folder)
-        files = os.listdir()
-        for item in files:
-            if os.path.isdir(item):
-                continue
-            bin_file = Parse_Files.is_binary(item)
-            if bin_file:
-                continue
-            with open(item, 'r') as f:
-                file_contents = f.read()
-                match = pattern.findall(file_contents)
-                Parse_Files.result_dict[item] = set(match)
-        Parse_Files.usage_count(Parse_Files.result_dict)
-
-    def is_binary(file_name):
-        ''' Fuction tries to open file as test and returns boolean'''
-        try:
-            with open(file_name, 'tr') as check_file:
-                check_file.read()
-                return False
-        except UnicodeDecodeError:
-            return True
-
-    def usage_count(parsed_data):
-        ''' Returns dictionary with tool number as key and
-        number of times used as value'''
-        all_file_tool_list = []
-        for key, value in parsed_data.items():
-            for item in value:
-                all_file_tool_list.append(item)
-        x = (s.strip('T') for s in all_file_tool_list)
-        y = (s.replace('T', '') for s in x)
-        new_tool_list = []
-        for item in y:
-            if item == '0' or item == '239' or int(item) > 300:
-                continue
-            new_tool_list.append(int(item))
-        new_tool_list.sort()
-        temp_dict = Counter(new_tool_list)
-        for k, v in temp_dict.items():
-            Parse_Files.new_dict[k] = v
-        Parse_Files.single_use(Parse_Files.new_dict)
-
-    def single_use(new_dict):
-        '''Returns dictionary with tool number as key and
-        file name as value for tools used in only one file'''
-        single = []
-        for keys, values in Parse_Files.new_dict.items():
-            if values == 1:
-                single.append(keys)
-        for tnum in single:
-            for k, v in Parse_Files.result_dict.items():
-                for i in v:
-                    if i == r'T{}'.format(tnum):
-                        Parse_Files.single_list[tnum] = k
-
-    def get_ct_number(in_data):
-        '''Gets CT number from master tool lis file.'''
-        Parse_Files.machine = choose_machine_combo.get()
-        tl = 'C:/Users/djennings/Google Drive/King Machine Cutting Tool List.xlsx'
-        wb = load_workbook(filename=tl)
-        sh1 = wb.active
-        t_data = {}
-        for key in in_data:
-            for cell1, cell2, cell3, cell4 in zip(sh1['A'], sh1['E'],
-                                                  sh1['C'], sh1['J']):
-                if cell1.value == key and cell2.value == Parse_Files.machine:
-                    t_data[key] = (cell3.value, cell4.value)
-        return t_data
-
-    def extract_programmer():
-        '''Retrieves programmer stats from files in folder'''
-        os.chdir(Parse_Files.folder_selected)
-        files = os.listdir()
-        dave_count = 0
-        john_count = 0
-        no_name = 0
-        for item in files:
-            if os.path.isdir(item):
-                continue
-            bin_file = Parse_Files.is_binary(item)
-            if bin_file:
-                continue
-            with open(item, 'r') as f:
-                file_data = f.read()
-                if 'DAVE' in file_data:
-                    dave_count += 1
-                elif 'JOHN' in file_data:
-                    john_count += 1
-                else:
-                    no_name += 1
-        dave_percent = (dave_count / (dave_count + john_count + no_name) * 100)
-        john_percent = (john_count / (dave_count + john_count + no_name) * 100)
-        no_name_percent = (no_name / (dave_count + john_count + no_name) * 100)
-        return (dave_percent, john_percent, no_name_percent,
-                dave_count, john_count, no_name)
-
-
 root = tk.Tk()
 root.title('Extract Tool List From Machine Library')
+
+# Class variables
+
+root.new_dict = {}
+root.single_list = {}
+root.result_dict = {}
+root.machine = ''
+root.folder_selected = ''
 
 
 def choose_folder(event):
     '''Opens folder selection dialog'''
-    Parse_Files.folder_selected = tk.filedialog.askdirectory()
-    file_listbox.insert(tk.END, Parse_Files.folder_selected)
+    root.folder_selected = tk.filedialog.askdirectory()
+    file_listbox.insert(tk.END, root.folder_selected)
     folder_pick.unbind('<ButtonRelease-1>')
     process.bind('<ButtonRelease-1>', extract)
 
 
 def extract(event):
-    '''Used to kick off processing after button selected.'''
-    pattern = re.compile(r'T\d+')
-    Parse_Files(Parse_Files.folder_selected, pattern)
+    os.chdir(root.folder_selected)
+    files = os.listdir()
+    pattern1 = re.compile(r'T\d+')
+    pattern2 = re.compile(r'411Z91\d+-\w.*')
+
+    match1 = filter(pattern2.search, files)
+    target_files = []
+    for item in match1:
+        if os.path.isdir(item):
+            continue
+        bin_file = is_binary(item)
+        if bin_file:
+            continue
+        target_files.append(item)
+    for item in target_files:
+        with open(item, 'r') as f:
+            file_contents = f.read()
+            match2 = pattern1.findall(file_contents)
+            root.result_dict[item] = set(match2)
+    usage_count(root.result_dict)
     write_to_spreadsheet()
+
+
+def is_binary(file_name):
+    ''' Fuction tries to open file as test and returns boolean'''
+    try:
+        with open(file_name, 'tr') as check_file:
+            check_file.read()
+            return False
+    except UnicodeDecodeError:
+        return True
+
+
+def usage_count(parsed_data):
+    ''' Returns dictionary with tool number as key and
+    number of times used as value'''
+    all_file_tool_list = []
+    for key, value in parsed_data.items():
+        for item in value:
+            all_file_tool_list.append(item)
+    x = (s.strip('T') for s in all_file_tool_list)
+    y = (s.replace('T', '') for s in x)
+    new_tool_list = []
+    for item in y:
+        if item == '0' or item == '239' or int(item) > 300:
+            continue
+        new_tool_list.append(int(item))
+    new_tool_list.sort()
+    temp_dict = Counter(new_tool_list)
+    for k, v in temp_dict.items():
+        root.new_dict[k] = v
+    single_use(root.new_dict)
+
+
+def single_use(new_dict):
+    '''Returns dictionary with tool number as key and
+    file name as value for tools used in only one file'''
+    single = []
+    for keys, values in root.new_dict.items():
+        if values == 1:
+            single.append(keys)
+    for tnum in single:
+        for k, v in root.result_dict.items():
+            for i in v:
+                if i == r'T{}'.format(tnum):
+                    root.single_list[tnum] = k
+
+
+def get_ct_number(in_data):
+    '''Gets CT number from master tool list file.'''
+    root.machine = choose_machine_combo.get()
+    tl = 'C:/Users/djennings/Documents/Programming/Python/SS_Master_Tool_List/King Machine Cutting Tool List.xlsx'
+    wb = load_workbook(filename=tl)
+    sh1 = wb.active
+    t_data = {}
+    for key in in_data:
+        for cell1, cell2, cell3, cell4 in zip(sh1['A'], sh1['E'],
+                                              sh1['C'], sh1['J']):
+            if cell1.value == key and cell2.value == root.machine:
+                t_data[key] = (cell3.value, cell4.value)
+    return t_data
+
+
+def extract_programmer():
+    '''Retrieves programmer stats from files in folder'''
+    os.chdir(root.folder_selected)
+    files = os.listdir()
+    dave_count = 0
+    john_count = 0
+    no_name = 0
+    for item in files:
+        if os.path.isdir(item):
+            continue
+        bin_file = is_binary(item)
+        if bin_file:
+            continue
+        with open(item, 'r') as f:
+            file_data = f.read()
+            if 'DAVE' in file_data:
+                dave_count += 1
+            elif 'JOHN' in file_data:
+                john_count += 1
+            else:
+                no_name += 1
+    dave_percent = (dave_count / (dave_count + john_count + no_name) * 100)
+    john_percent = (john_count / (dave_count + john_count + no_name) * 100)
+    no_name_percent = (no_name / (dave_count + john_count + no_name) * 100)
+    return (dave_percent, john_percent, no_name_percent,
+            dave_count, john_count, no_name)
 
 
 def max_length(eval_string):
@@ -201,7 +208,7 @@ def write_to_spreadsheet():
     wb.add_named_style(highlight)  # Register named style
     sh1 = wb.active
     sh1.title = 'Tool Usage Frequency'
-    sh1.append(['Tool Number', 'Times Used', 'CT Number', 'Description'])
+    sh1.append(['Tool Number', 'Times Used', 'CT Number', 'Description', 'Holder'])
     sh1['A1'].font = Font(bold=True, size=11)
     sh1['A1'].border = Border(left=bd, top=bd, right=bd, bottom=bd)
     sh1['A1'].alignment = Alignment(horizontal='center')
@@ -214,14 +221,17 @@ def write_to_spreadsheet():
     sh1['D1'].font = Font(bold=True, size=11)
     sh1['D1'].border = Border(left=bd, top=bd, right=bd, bottom=bd)
     sh1['D1'].alignment = Alignment(horizontal='center')
+    sh1['E1'].font = Font(bold=True, size=11)
+    sh1['E1'].border = Border(left=bd, top=bd, right=bd, bottom=bd)
+    sh1['E1'].alignment = Alignment(horizontal='center')
     rnum = 2
     sh1.column_dimensions['A'].width = (11.95)
     sh1.column_dimensions['B'].width = (10.6)
     sh1.column_dimensions['C'].width = (10)
     col_width2 = 0
-    sh1_ct_data = Parse_Files.get_ct_number(Parse_Files.new_dict)
-    for keys, values in Parse_Files.new_dict.items():
-        sh1_tool_list_data = sh1_ct_data[keys]
+    sh1_ct_data = get_ct_number(root.new_dict)
+    for keys, values in root.new_dict.items():
+        sh1_tool_list_data = sh1_ct_data[keys]  #keys is tool number, value is times used
         sh1.cell(row=rnum, column=1).value = int(keys)
         sh1.cell(row=rnum, column=2).value = int(values)
         sh1.cell(row=rnum, column=1).style = 'highlight'
@@ -252,12 +262,12 @@ def write_to_spreadsheet():
     sh2['D1'].border = Border(left=bd, top=bd, right=bd, bottom=bd)
     sh2['D1'].alignment = Alignment(horizontal='center')
     rnum = 2
-    col_width = (max_length(Parse_Files.single_list))
+    col_width = (max_length(root.single_list))
     sh2.column_dimensions['A'].width = (11.95)
     sh2.column_dimensions['B'].width = (col_width * 1.125)
     sh2.column_dimensions['C'].width = (10)
     col_width3 = 0
-    for keys, values in Parse_Files.single_list.items():
+    for keys, values in root.single_list.items():
         sh2_tool_list_data = sh1_ct_data[keys]
         sh2_ct_num, sh2_description = sh2_tool_list_data
         sh2.cell(row=rnum, column=1).value = int(keys)
@@ -275,7 +285,7 @@ def write_to_spreadsheet():
     # ----------------------------------------------------------
     sh3 = wb.create_sheet(title='Programmer Stats')
     wb.active = 3
-    prog_stat = Parse_Files.extract_programmer()
+    prog_stat = extract_programmer()
     dp, jp, np, dc, jc, nc = prog_stat
     sh3.append(['Programmer', '# Programmed', '% Programmed'])
     sh3['A1'].font = Font(bold=True, size=11)
@@ -309,7 +319,7 @@ def write_to_spreadsheet():
     sh3.column_dimensions['B'].width = (15)
     sh3.column_dimensions['C'].width = (16)
     save_name = (('{}/{} Tool Usage Data.xlsx').format
-                 (Parse_Files.folder_selected, Parse_Files.machine))
+                 (root.folder_selected, root.machine))
     wb.save(save_name)
     file_listbox.insert(tk.END, 'Operation Complete')
     file_listbox.see(tk.END)
